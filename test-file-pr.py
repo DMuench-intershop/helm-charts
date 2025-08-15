df170dd4e542f60ca452cf65ba1b33242aa1aa70
Dominik Münch <DMuench@intershop.com>
2025-08-15 11:35:43 +0200

    feat: added a test file

---
+++ test-file-pr.py
@@ -0,0 +1,8 @@
+def main():
+    print("Welcome to the user console dialog!")
+    name = input("What is your name? ")
+    age = inpt("How old are you? ")
+    print(f"Hello, {name}! You are {age} years old.")
+
+if __name__ == "__main__":
+    main()
